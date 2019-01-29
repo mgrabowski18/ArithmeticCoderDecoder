308738a6a7a927c81bb2924f162119487efe483f
Marcin Grabowski <mgrabowski18@gmail.com>
2019-01-29 09:22:24 +0100

    Version 1.0

--- src/Decoder/Main.java
+++ src/Decoder/Main.java
@@ -1,4 +1,7 @@
 package Decoder;
 
 public class Main {
+    public static void main(String[] args) {
+        Model m = new Model();
+    }
 }
--- src/Decoder/Model.java
+++ src/Decoder/Model.java
@@ -1,13 +1,39 @@
 package Decoder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Model {
     private Map<Character, Double> dictionary;  //map for letter and her probability
-    private String[][] stringToDecode;    //input string to code
+    private String[] stringToDecode;    //input string to code
+    private int counter = 0;
 
     public Model()
     {
-        stringToDecode = new String() [][];
+        readCodedString();
+    }
+
+    private void readCodedString()
+    {
+        try {
+            File file = new File("src\\resources\\CodedString.txt");
+            Scanner in = new Scanner(file);
+            while(in.hasNext())
+                counter++;
+            stringToDecode = new String[counter];
+            counter=0;
+            while(in.hasNext())
+            {
+                stringToDecode[counter]=in.nextLine();
+                System.out.println(stringToDecode[counter]);
+                counter++;
+            }
+            in.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
